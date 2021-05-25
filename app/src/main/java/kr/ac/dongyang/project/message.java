d1e64d300b5250f7700bcaa4e8de085cf6b65b33
hyeonmin <hyeonmin97@naver.com>
2021-05-25 23:34:13 +0900

    init : message.java
    message.java 파일 생성

---
+++ app/src/main/java/kr/ac/dongyang/project/message.java
@@ -0,0 +1,14 @@
+package kr.ac.dongyang.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class message extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.message);
+    }
+}
