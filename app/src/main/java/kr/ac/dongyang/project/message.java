41454cc3f1dd6eb30e9abbb221c2c9fdf4701865
hyeonmin <hyeonmin97@naver.com>
2021-05-29 04:12:38 +0900

    FIX : message.java

--- app/src/main/java/kr/ac/dongyang/project/message.java
+++ app/src/main/java/kr/ac/dongyang/project/message.java
@@ -1,14 +1,53 @@
 package kr.ac.dongyang.project;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class message extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.message);
+        Button btnY = (Button)findViewById(R.id.btnY);
+        Button btnN = (Button)findViewById(R.id.btnN);
+        btnY.setOnClickListener(new View.OnClickListener(){//넘어졌을 경우
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(getApplicationContext(),"넘어짐",Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(), tcp.class);
+                intent.putExtra("button", "btnY");
+                //메시지 보내기 구현
+                
+                startService(intent);
+                finish();
+            }
+        });
+        btnN.setOnClickListener(new View.OnClickListener(){//넘어지지 않았을경우
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(getApplicationContext(),"안넘어짐",Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(), tcp.class);
+                intent.putExtra("button", "btnN");
+                startService(intent);
+                finish();
+            }
+        });
+        
+    }
+
+    @Override
+    public void onBackPressed() {
+        //Toast.makeText(this, "Back button pressed.", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(getApplicationContext(), tcp.class);
+        intent.putExtra("button", "btnN");
+        startActivity(intent);
+        finish();
+        //super.onBackPressed();
     }
 }
